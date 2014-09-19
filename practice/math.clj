ca32a8da5c8c6f256c02f2a7e1f68dfd3dfd34a7
Sabda PS <squest@users.noreply.github.com>
2014-09-20 00:19:06 +0700

    Another practice

---
+++ practice/math.clj
@@ -0,0 +1,4 @@
+;; Implement these mathematical routines in clojure using pure recursion & loop-recur
+
+;; sum of a coll, product of a coll, factorial, fibo, fibolist, prime?, primes-under, primes-range, next-prime, prev-prime
+;; maxi of a coll, mini of a coll, factors (returns all factors of an integer).
