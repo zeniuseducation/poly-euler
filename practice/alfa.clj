81c6d5355ea216e4e73075243d15db0debc41387
arie741 <arie.pranasakti@zeniuseducation.com>
2014-09-19 23:07:13 +0700

    Update alfa.clj

--- practice/alfa.clj
+++ practice/alfa.clj
@@ -1,4 +1,12 @@
 (ns euler.practice.alfa)
 
+;;take''
+(defn a-take [a b]
+  (loop [la (dec a)
+         lb '()]
+    (if (= la -1)
+      lb
+      (recur (dec la) (conj lb (nth b la))))))
+
 ;; Reimplementing Clojure in pure recursion
 ;; last, butlast, keep, map, take, take-while, remove, drop, drop-while, distinct, range, for
