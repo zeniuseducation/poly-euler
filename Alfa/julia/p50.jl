6beb0cb746cab6bfe7d9ed670ff4adea5ddf0cdd
Sabda PS <@sabdaps>
2015-03-16 06:44:31 +0700

    more problems

--- Alfa/julia/p50.jl
+++ Alfa/julia/p50.jl
@@ -253,6 +253,258 @@ function sol13 ()
     int (apply (string ,(sub ( collect(string (sum (map (BigInt,map (chop,open (readlines,"p13.txt")))))), 1:10))))
 end
 
+function sol14 (lim :: Int)
+    function twist (n :: Int)
+        if n == 1
+            return 1
+        elseif iseven (n)
+            return 1 + twist (div (n,2))
+        else
+            return 1 + twist (3*n + 1)
+        end
+    end
+    tmp :: Int, itmp :: Int = 0,1
+    for i = 2:lim
+        cres = twist (i)
+        if cres > tmp
+            tmp = cres
+            itmp = i
+        end
+    end
+    return itmp
+end
+
+function maxby (f,xs)
+    res = f (first (xs))
+    ires = first (xs)
+    for i in xs
+        tmp = f (i)
+        if tmp > res
+            res = tmp
+            ires = i
+        end
+    end
+    return ires
+end
+
+
+function sol14b (lim :: Int)
+    gc_disable ()
+    function twist (n :: Int)
+        if n == 1
+            return [1,1]
+        elseif iseven (n)
+            (_,r) = twist (div (n,2))
+            return [n,1+r]
+        else
+            (_,r) = twist (3*n + 1)
+            return [n,1+r]
+        end
+    end
+    res = maxby (x -> x [2], pmap (twist,600001:2:lim))
+    gc_enable ()
+    return res 
+end
+
+function sol15 (n :: Int)
+    res :: Int = 0
+    for i = 0:n
+        tmp :: Int = binomial (n,i)
+        res += tmp*tmp
+    end
+    return res
+end
+
+
+
+function sol16 (n::Int)
+    sumdig (^(BigInt (2), n))
+end
+
+function numcol (n::BigInt)
+    map (x -> int (x) - 48, collect ( string (n)))
+end
+
+function words (n :: Int)
+    refs = ["one", "two", "three","four", "five","six","seven","eight","nine"]
+    refp = ["", "twenty", "thirty","forty","fifty","sixty","seventy", "eighty", "ninety"]
+    refu = vcat (refs,["ten","eleven","twelve","thirteen", "fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"])
+    if 100 <= n <= 999
+        dep = div (n,100)
+        bel = n % 100
+        if bel == 0
+            return (refs [dep]) * "hundred"
+        else
+            return (refs [dep]) * "hundredand" * (words (n % 100))
+        end
+    elseif 20 <= n <= 99
+        dep = div (n,10)
+        return (refp [dep]) * (words (n % 10))
+    elseif 0 == n
+        return ""
+    elseif n == 1000
+        return "onethousand"
+    else
+        return refu[n]
+    end
+end
+
+function sol17 (lim::Int)
+    sum (map (x -> length(words (x)),1:lim))
+end
+
+function readp18 ()
+    tmp = map (chop, open (readlines,"p18.txt"))
+    tmp = map (x -> split (x, ' '), tmp)
+    tmp = map (x -> map (int,x), tmp)
+end
+
+function sol18 ()
+    tmp = reverse (readp18 ())
+    literate = length (tmp) - 1
+    for i = 1:literate
+        for j = 1:(length (tmp [i+1]))
+            resi = tmp [i]
+            a = resi [j]
+            b = resi [j+1]
+            if a > b
+                resa = tmp [i+1] [j]
+                tmp [i+1] [j] = resa + a
+            else
+                resa = tmp [i+1] [j]
+                tmp [i+1] [j] = resa + b
+            end
+        end
+    end
+    return first (last (tmp))
+end
+
+function sumdig (n :: BigInt)
+    res :: Int = 0
+    i :: BigInt = n
+    while i >= 10
+        res += i % 10
+        i = div (i,10)
+    end
+    return res+i
+end
+
+function fact (n::BigInt)
+    n == 1 ? 1 : n * factorial (n-1)
+end
+
+function sol20 (n)
+    sumdig (fact (BigInt (n)))
+end
+
+function pdivisors (n::Int)
+    res :: Int = 1
+    lim :: Int = isqrt (n)
+    if iseven (n)
+        for i = 2:lim
+            if 0 == n % i
+                if i*i == n
+                    res += i
+                else
+                    res += i + div (n,i)
+                end
+            end
+        end
+    else
+        for i = 3:2:lim
+            if 0 == n % i
+                if i*i == n
+                    res += i
+                else
+                    res += i + div (n,i)
+                end
+            end
+        end
+    end
+    return res 
+end
+
+function sol21 (lim::Int)
+    function isamic (n::Int)
+        tmp :: Int = pdivisors (n)
+        if tmp > n
+            stmp :: Int = pdivisors (tmp)
+            return stmp == n ? (tmp+n) : 0
+        else
+            return 0
+        end
+        
+    end
+
+    res :: Int = 0
+    for i = 1:lim
+        res += isamic (i)
+    end
+    return res
+end
+
+function readp22 ()
+    tmp = open (readall, "p22.txt")
+    tmp = chop (tmp)
+    tmp = split (tmp, ",")
+    tmp = map (x -> reverse (chop (reverse (chop (x)))) , tmp)
+    tmp = sort (tmp)
+end
+
+function getscore (nama :: String)
+    refs = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    sum (map (x -> findfirst (refs,x), collect (nama)))
+end
+
+function sol22 ()
+    tmp = readp22()
+    res :: Int = 0
+    for i = 1:length (tmp)
+        res += i * getscore (tmp [i])
+    end
+    return res 
+end
+
+function sol22b ()
+    tmp = readp22 ()
+    sum (map (x-> findfirst (tmp,x) * getscore (x), tmp))
+end
+
+function sol23 (lim::Int)
+    abuns = falses (lim)
+    sumabuns = trues (lim)
+    for i = 1:lim
+        tmp = pdivisors (i)
+        if tmp > i
+            abuns [i] = true
+        end
+    end
+    res :: Int = div(lim * (lim+1),2)
+    for i = 12:lim
+        if abuns [i]
+            j :: Int = i 
+            tres = (i+j) <= lim
+            while tres
+                if abuns [j]
+                    if sumabuns [i+j]
+                        res -= (i+j)
+                        sumabuns [i+j] =false
+                    end
+                end
+                j += 1
+                tres = (i+j) <= lim
+            end
+        end
+    end
+    return res
+end
+
+
+
+
+
+
+
 
 
 
