641b84e312577b661383a8c85495cbff777f30b6
squest <sabdaps@gmail.com>
2015-06-18 23:33:39 +0700

    update

--- Alfa/src/alfa/under200/four.clj
+++ Alfa/src/alfa/under200/four.clj
@@ -19,3 +19,40 @@
        (let [res (let [resi (* i j)]
                    (if (palin? resi) resi 0))]
          (max res (maxi i (- j 1)) (maxi (- i 1) (- i 2))))))))
+
+(defn lamina
+  [lim odd-even?]
+  (let [oe (if odd-even? 3 4)]
+    (count (for [i (map #(* % %) (range oe lim 2))
+                 j (map #(* % %) (range (- i 2) (- oe 2) -2))
+                 :let [diff (- i j)]
+                 :while (<= diff lim)]
+             diff))))
+
+(defn ^long sol173
+  [^long lim]
+  (let [llim (int (quot (+ lim 4) 4))]
+    (loop [i (int 3) res (int 0)]
+      (if (> i llim)
+        res
+        (recur (+ i 1)
+               (+ res
+                  (let [is (* i i)]
+                    (loop [j (int (- i 2)) resj (int 0)]
+                      (let [diff (- is (* j j))]
+                        (if (or (> diff lim)
+                                (if (odd? i)
+                                  (== j -1)
+                                  (== j 0)))
+                          resj
+                          (recur (- j 2)
+                                 (+ 1 resj))))))))))))
+
+(defn redux
+  ([f [x & xs]]
+     (redux f x xs))
+  ([f x [l & ls]]
+     (if l
+       (let [acc (f x l)]
+        (cons acc (lazy-seq (redux f acc ls))))
+       [])))
