d774b6336c397c8d28a6e14d36546423d309c1cb
squest <sabdaps@gmail.com>
2015-11-21 20:11:04 +0700

    works but slow

--- Alfa/src/alfa/five.clj
+++ Alfa/src/alfa/five.clj
@@ -4,6 +4,35 @@
    [alfa.common :refer :all]
    [clojure.set :refer [intersection difference]]))
 
+(defn run [f & args]
+  (dotimes [i 5]
+    (let [res (time (apply f args))]
+      (println res))))
+
+(defn ^long p50 [^long lim]
+  (let [refs (boolean-array lim true)
+        llim (inc (int (Math/sqrt lim)))
+        primes (sieve lim)]
+    (loop [i (int 2)]
+      (if (aget refs i)
+        (when (<= i llim)
+          (do (loop [j (int (* i i))]
+                (when (< j lim)
+                  (aset refs j false)
+                  (recur (+ j i))))
+              (recur (+ i 1))))
+        (recur (+ i 1))))
+    (->> primes
+         (iterate rest)
+         (take-while not-empty)
+         (mapcat #(->> (iterate butlast %)
+                       (take-while not-empty)
+                       (keep (fn [x]
+                               (let [res (reduce + x)]
+                                 (when (< res lim)
+                                   [(count x) res]))))))
+         (filter #(aget refs (second %)))
+         (apply max-key first))))
 
 
 
